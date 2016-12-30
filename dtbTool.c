557fde5e4e50f029dcf2a243c50bf091dc877ed1
Wilhansen Li <wil@byimplication.com>
2016-12-30 12:26:05 +0800

    Minor formatting fix

--- dtbTool.c
+++ dtbTool.c
@@ -439,7 +439,7 @@ int main(int argc, char **argv)
 
     log_info("\nGenerating master DTB... ");
 
-    out_fd = open(output_file, O_WRONLY|O_CREAT | O_TRUNC, S_IRUSR|S_IWUSR);
+    out_fd = open(output_file, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
     if (!out_fd < 0) {
         log_err("Cannot create '%s'\n", output_file);
         rc = RC_ERROR;
